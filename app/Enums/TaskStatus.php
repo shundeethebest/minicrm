ed186808bdcc866274406b93258ba84056aeaf41
Shundee Navarroza <32363808+shundeethebest@users.noreply.github.com>
2024-11-04 16:05:16 +0800

    <add_main/taskCRUD> - add the database, model, Enum, Factory, and Seeder of Task CRUD : On-hold

---
+++ app/Enums/TaskStatus.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+enum TaskStatus: string
+{
+    case OPEN = 'open';
+    case IN_PROGRESS = 'in progress';
+    case PENDING = 'pending';
+    case WAITING_CLIENT = 'waiting client';
+    case BLOCKED = 'blocked';
+    case CLOSE = 'closed';
+
+}
